6587c9fc2aba2bd46156c11db524aeb46effc10e
Boris Kaul <localvoid@gmail.com>
2014-11-05 14:57:18 +0600

    Github page

--- README.md
+++
@@ -1,5 +0,0 @@
-# TodoMVC
-
-TodoMVC example ([Liquid](https://github.com/localvoid/liquid) library).
-
-[View in action](https://localvoid.github.io/liquid/)
--- lib/src/utils.dart
+++
@@ -1,4 +0,0 @@
-part of todomvc;
-
-/// I really don't know how to explain how this works. It's Pure magic.
-String pluralize(count, word) => count == 1 ? word : word + 's';
